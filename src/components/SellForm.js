598649951ae8d1015c3bac003f52d75b8d78d431
lexeach <80155340+lexeach@users.noreply.github.com>
2022-07-31 19:50:06 +0530

    Update SellForm.js

--- src/components/SellForm.js
+++ src/components/SellForm.js
@@ -68,7 +68,7 @@ class SellForm extends Component {
         </div>
         <div className="mb-5">
           <span className="float-left text-muted">Exchange Rate</span>
-          <span className="float-right text-muted">1 EXN = 1 USDT</span>
+          <span className="float-right text-muted">1 EXN = 2 USDT</span>
         </div>
         <button className="swapButton">SWAP!</button>
       </form>
